409563a61f5aeab939e27924cf4561caa5eaf79f
ARYA202020 <94689988+ARYA202020@users.noreply.github.com>
2021-11-19 17:53:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var ball
+
+function setup() {
+  createCanvas(400,400);
+ball=createSprite(200,200,100,100)
+
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+  if (keyDown("right")){
+  ball.x=ball.x+5;
+
+  }
+  
+
+}
+
+
+
+
